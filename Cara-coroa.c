86f047f1f8531b6521c2fa02cc074a05692d66c4
Torist12 <daudtpennawilliam@gmail.com>
2026-05-11 21:24:27 -0300

    Cara ou coroa added

---
+++ Cara-coroa.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+    srand(time(NULL));
+
+    int moeda = rand() % 2;
+    int usr;
+    int jogar =1;
+    while (jogar == 1) {
+
+    printf("Digite 0 para cara e 1 para coroa:\n");
+    scanf("%d", &usr);
+
+    if (moeda ==0) {
+        printf("cara\n");
+    }
+    else{
+        printf("coroa\n");
+    }
+    if (usr == moeda) {
+        printf("Você acertou!\n");
+    }
+    else {
+        printf("Você errou!\n");
+    }
+
+    printf("deseja jogar novamente? 1 = sim, 0 = não;\n");
+    scanf("%d", &jogar);
+    }
+     return 0;
+    }
